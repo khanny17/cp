8b8e61e8435e466fa8b2b2d3f8e9a2ef50c859db
Kevin Hannigan <kph1958@rit.edu>
2018-09-21 17:28:41 -0700

    remove micro microservices

--- preferences/preferences_model.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const schema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  school: { type: mongoose.Schema.Types.ObjectId, ref: 'School' },
-});
-
-module.exports = mongoose.model('Preferences', schema);
